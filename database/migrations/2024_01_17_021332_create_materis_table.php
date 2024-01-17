22e9108d4493781d5a4b65e24bfe44d38231b162
Yusuf Project <yusufforproject@gmail.com>
2024-01-17 09:27:38 +0700

    api content & materi

--- database/migrations/2024_01_17_021332_create_materis_table.php
+++ database/migrations/2024_01_17_021332_create_materis_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contents', function (Blueprint $table) {
+        Schema::create('materis', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->string('image')->nullable();
+            $table->string('image')->nullable;
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contents');
+        Schema::dropIfExists('materis');
     }
 };
